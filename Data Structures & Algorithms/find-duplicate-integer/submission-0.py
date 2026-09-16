85cda2600c7cefac571361f980e44f3fc0deec70
gangudsp79-crypto <gangudsp79@gmail.com>
2026-09-16 12:22:44 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        l=[]
+        for i in nums:
+            
+            if i in l:
+                return i
+            l.append(i)
+        
